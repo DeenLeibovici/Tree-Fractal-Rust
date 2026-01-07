fccc2e60a13ec48976a6c13f7942fcb611720cf5
DeenLeibovici <deenleibovici@ufl.edu>
2026-01-06 22:47:20 -0500

    First Commit

---
+++ src/fractal.rs
@@ -0,0 +1,84 @@
+use macroquad::{prelude::*, rand::gen_range};
+use std::f32::consts::PI;
+use macroquad::ui;
+
+#[derive(Clone)]
+struct TreeParams {
+    iteration : i32,
+    total_iterations : i32,
+    angle : f32,
+    ratio : f32,
+    offset : f32,
+    pos : Vec2,
+    scale : f32,
+    color : Color,
+    disco_mode : bool
+}
+
+fn draw_tree_helper(mut params : TreeParams){
+    if params.iteration == params.total_iterations{
+        return;
+    }
+    let scale_factor = params.ratio.powf(params.iteration as f32) * params.scale;
+    
+    let x2= params.offset.cos() * scale_factor + params.pos.x;
+    let y2 = -params.offset.sin() * scale_factor + params.pos.y;
+
+    draw_line(params.pos.x, params.pos.y,  x2, y2, 2.5*params.ratio.powf(params.iteration as f32) + 0.75, params.color);
+    params.iteration += 1;
+    params.pos = Vec2 { x: x2, y: y2 };
+    params.offset += params.angle;
+
+    if params.disco_mode{
+        let random_color : Color = Color{r : gen_range(0.0, 1.0),g : gen_range(0.0, 1.0), b: gen_range(0.0, 1.0), a: 1.0};
+        params.color = random_color;
+    }
+
+    draw_tree_helper(params.clone());
+    params.offset -= 2.0 * params.angle;
+    draw_tree_helper(params.clone());
+}
+
+pub fn draw_tree(total_iterations : i32, angle : f32, ratio : f32, pos : Vec2, scale : f32, diso_mode : bool){
+    // draw_line(0.0, screen_height()/2.0, screen_width(), screen_height()/2.0, 3.0, BLUE);
+    // draw_line(screen_width() / 2.0, 0.0, screen_width() / 2.0, screen_height(), 3.0, RED);
+    let params = TreeParams {
+        iteration : 0,
+        total_iterations : total_iterations,
+        angle : angle,
+        ratio : ratio,
+        offset : PI / 2.0,
+        pos : pos,
+        scale : scale,
+        color : GREEN,
+        disco_mode : diso_mode
+    };
+
+    draw_tree_helper(params.clone());
+
+}
+
+pub fn mouse_controls(pos : &mut Vec2, start : &mut Vec2, old_pos : &mut Vec2, scale: &mut f32){
+        if is_mouse_button_pressed(MouseButton::Left){
+            let m = mouse_position();
+            start.x = m.0;
+            start.y = m.1;
+
+            *old_pos = *pos;
+        }
+        else if is_mouse_button_down(MouseButton::Left) && !ui::root_ui().is_mouse_captured(){ 
+            let m = mouse_position();
+            let delta_x = m.0 - start.x;
+            let delta_y = m.1 - start.y;
+            
+            pos.x = old_pos.x + delta_x;
+            pos.y = old_pos.y + delta_y;
+        }
+
+        let wheel = mouse_wheel().1;
+        if wheel != 0.0{
+            *scale += wheel * 10.0;
+        }
+        
+
+}
---
+++ src/main.rs
@@ -0,0 +1,65 @@
+use macroquad::prelude::*;
+use macroquad::ui;
+use macroquad::ui::Skin;
+use std::f32::consts::PI;
+mod fractal;
+
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "Fractal".to_string(),
+        window_width: 1000,
+        window_height: 1000,
+        sample_count: 16,
+        ..Default::default()
+    }
+}
+fn ui_skin() -> Skin {
+    let label_style_built  = ui::root_ui()
+                                    .style_builder()
+                                    .text_color(WHITE)
+                                    .build();
+
+    Skin {label_style: label_style_built, 
+        ..ui::root_ui().default_skin() }
+}
+
+fn immediate_ui(skin: &Skin, angle : &mut f32, ratio : &mut f32, iterations : &mut f32, disco_toggle : &mut bool){
+        ui::root_ui().push_skin(&skin);
+                ui::root_ui().label(None, "Angle(rad)");
+                ui::root_ui().slider(1, "[-10 .. 10]", -10f32..10f32, angle);
+
+                ui::root_ui().label(None, "Ratio");
+                ui::root_ui().slider(2, "[-10 .. 10]", -10f32..10f32, ratio);
+
+                ui::root_ui().label(None, "Angle(rad)");
+                ui::root_ui().slider(3, "[0 .. 20]", 0f32..20f32, iterations);
+
+                if ui::root_ui().button(None, if *disco_toggle { "[x] Disable Disco Mode" } else { "[ ] Enable Disco Mode" }) {
+                    *disco_toggle = !*disco_toggle;
+                }
+                ui::root_ui().pop_skin();
+                *iterations = iterations.round();
+}
+#[macroquad::main(window_conf)]
+async fn main() {
+    let mut angle : f32 = (2.0*PI)/11.0;
+    let mut ratio : f32 = 0.75;
+    let mut scale : f32 = 100.0;
+    let mut iterations : f32 = 10.0;
+
+    let mut pos : Vec2 = Vec2 { x: screen_width() / 2.0, y: screen_height() / 2.0 + 100.0};
+    let mut start : Vec2 =  Vec2::ZERO;
+    let mut old_pos : Vec2 =  Vec2::ZERO;
+    let mut disco_toggle : bool = false;
+    let skin = ui_skin();
+    loop {
+        fractal::mouse_controls(&mut pos, &mut start, &mut old_pos, &mut scale);
+        
+        immediate_ui(&skin, &mut angle, &mut ratio, &mut iterations, &mut disco_toggle);
+
+        fractal::draw_tree(iterations as i32, angle, ratio, pos, scale, disco_toggle);
+    
+        // draw_fps();
+        next_frame().await;
+    }
+}
